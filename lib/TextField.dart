e7420b4359c5b6a830ca655cc004f23bac342ad8
Kartikey2011yadav <nykay10968@gmail.com>
2024-05-07 12:19:51 +0530

    Text Field Example

---
+++ lib/TextField.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class TextField_ extends StatelessWidget {
+  const TextField_({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.circular(25),
+            ),
+            padding: const EdgeInsets.all(90),
+            child: const Text(
+              "Hello this is a text 2.0",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
