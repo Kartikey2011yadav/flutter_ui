25c75500f64f152e6f5f473388ed2cd5bd7e0b07
Kartikey2011yadav <nykay10968@gmail.com>
2024-06-03 16:24:06 +0530

    update

--- lib/navigation_home.dart
+++ lib/navigation_home.dart
@@ -10,61 +10,84 @@ class NavigationEx extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       routes: {
-        '/home': (context) => const NavigationHome(),
-        '/settings': (context) => const NavigationSettings(),
-        '/profile' : (context) => const NavigationProfile(),
+        '/home': (context) =>  NavigationHome(),
+        '/settings': (context) =>  NavigationSettings(),
+        '/profile': (context) =>  NavigationProfile(),
       },
-      home: const NavigationHome(),
+      home:  NavigationHome(),
     );
   }
 }
 
 class NavigationHome extends StatelessWidget {
-  const NavigationHome({super.key});
+  final List _pages =[
+    NavigationHome(),
+    NavigationSettings(),
+    NavigationProfile(),
+
+  ];
+   NavigationHome({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Home Page"),
-          backgroundColor: Colors.blueAccent,
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.purpleAccent[50],
+        child: Column(
+          children: [
+            const DrawerHeader(
+                child: Icon(
+              Icons.home,
+              size: 48,
+            )),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text("H O M E"),
+              onTap: () {
+                Navigator.pop(context);
+                // Navigator.pushNamed(context, '/home');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text("P R O F I L E"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/profile');
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text("S E T T I N G S"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/settings');
+              },
+            ),
+          ],
         ),
-        drawer: Drawer(
-          backgroundColor: Colors.purpleAccent[50],
-          child: Column(
-            children: [
-              const DrawerHeader(
-                  child: Icon(
-                Icons.home,
-                size: 48,
-              )),
-              ListTile(
-                leading: const Icon(Icons.home),
-                title: const Text("H O M E"),
-                onTap: () {
-                  Navigator.pop(context);
-                  // Navigator.pushNamed(context, '/home');
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.person),
-                title: const Text("P R O F I L E"),
-                onTap: () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, '/profile');
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.settings),
-                title: const Text("S E T T I N G S"),
-                onTap: () {
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, '/settings');
-                },
-              ),
-            ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.lightBlueAccent[100],
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
           ),
-        )
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+      ),
     );
   }
 }
